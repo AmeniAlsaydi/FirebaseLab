e2d6451aca0155301b7f70bb0f165b861b9dd64e
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-03-03 13:21:50 -0500

    User can change display name

--- FirebaseLab/Controllers/AppViewController.swift
+++ FirebaseLab/Controllers/AppViewController.swift
@@ -7,24 +7,94 @@
 //
 
 import UIKit
+import FirebaseAuth
+
+enum ProfileState {
+    case editing
+    case notEditing
+}
+
 
 class AppViewController: UIViewController {
 
+    @IBOutlet weak var displayNameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    // text feilds
+    @IBOutlet weak var displayTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var updateButton: UIButton!
+    
+    private var profileState = ProfileState.notEditing
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        laodUserInfo()
+        toggleProfileState()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func laodUserInfo() {
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+        
+        displayNameLabel.text = user.displayName ?? "No display name"
+        phoneLabel.text = user.phoneNumber  ?? "No phone number"
+        emailLabel.text = user.email ?? "No email"
+        
+        displayTextField.text = user.displayName
+        numberTextField.text = user.phoneNumber
+        emailTextField.text = user.email
+        
     }
-    */
-
+    
+    private func toggleProfileState() {
+        if profileState == .notEditing {
+            displayTextField.isHidden = true
+            numberTextField.isHidden = true
+            emailTextField.isHidden = true
+            updateButton.isHidden = true
+        } else if profileState == .editing {
+            displayTextField.isHidden = false
+            numberTextField.isHidden = false
+            emailTextField.isHidden = false
+            updateButton.isHidden = false
+        }
+    }
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        profileState = .editing
+        toggleProfileState()
+        
+    }
+    
+    @IBAction func updateButtonPressed(_ sender: UIButton) {
+        profileState = .notEditing
+        toggleProfileState()
+        
+        // create request to update profile
+        
+        guard let displayName = displayTextField.text, !displayName.isEmpty, let email = emailTextField.text, !email.isEmpty else {
+            print("missing fields")
+            return
+        }
+        
+        let request = Auth.auth().currentUser?.createProfileChangeRequest()
+        request?.displayName = displayName
+        
+        request?.commitChanges(completion: { [unowned self] (error) in
+                  if let error = error  {
+                      self.showAlert(title: "Profile change", message: "Error changing an alert: \(error) ")
+                  } else {
+                      self.showAlert(title:"Profile change" , message: "Profile successfully created")
+                    self.displayNameLabel.text = displayName
+                  }
+              })
+        
+    }
+    
 }
---
+++ FirebaseLab/Supporting files/AlertExt.swift
@@ -0,0 +1,20 @@
+//
+//  AlertExt.swift
+//  FirebaseLab
+//
+//  Created by Amy Alsaydi on 3/3/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    public func showAlert(title: String?, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
